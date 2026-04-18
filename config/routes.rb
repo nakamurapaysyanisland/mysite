393f605e3ec5267027404b3871fbd14a018bc79a
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-04-18 05:45:48 +0000

    add: headerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # HTTPメソッド 'URL' => 'コントローラ#アクション'
-  
-  get 'top' => 'homes#top' # トップページのルーティング 
+
+  get '/' => 'homes#top', as: 'top' # トップページのルーティング  asオプション：名前付きヘルパーとしてtop_pathやtop_urlが作成される
 end
