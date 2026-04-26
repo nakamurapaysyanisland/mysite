222c0169d6304631d8e261f647f060d7902b2ba6
nakamurapaysyanisland <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-04-26 13:18:42 +0000

    feat: deviseが使用される前にconfigure_permitted_parametersメソッドが実行されうように記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configre_permitted_parameters, if: :deise_controller?
+
+  protected
+
+  def configre_permitted_paramerters
+    devise_paramerter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
